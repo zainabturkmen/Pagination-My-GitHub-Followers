769a901616dce1a99a766ed7991960b718122e13
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-06 10:39:20 +0430

    adding some changes

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ import displayFollowers from './displayFollowers.js'
 import paginate from './paginate.js'
 import displayButtons from './displayButtons.js'
 
+const title = document.querySelector(".section-title h1");
+
 const init  = async (){
     const followers = await fetchFollowers();
     console.log(followers);
-}
+};
 
 window.addEventListener("load", init);
