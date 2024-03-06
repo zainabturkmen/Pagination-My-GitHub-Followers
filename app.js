1a5e4433836a138bf1325daefa0a590050820767
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-06 14:33:55 +0430

    adding some changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import displayButtons from './displayButtons.js'
 const title = document.querySelector(".section-title h1");
 const btnContainer = document.querySelector(".btn-container");
 
-let 
+let index = 0;
 const init  = async () => {
     const followers = await fetchFollowers();
     displayFollowers(paginate(followers)[0]);
