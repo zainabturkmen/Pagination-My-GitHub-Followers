9223e1e4c409de7ba53d0a2e842dd2301f90ceec
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-06 13:57:14 +0430

    adding some changes

--- paginate.js
+++ paginate.js
@@ -1,6 +1,6 @@
 const paginate = (followers) => {
     const itemsPerPage = 10;
-    const numberOfPages = followers.length 
+    const numberOfPages = followers.length;
 }
 
 export default paginate
