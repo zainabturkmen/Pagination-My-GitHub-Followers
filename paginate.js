c2aee09b799a8b363366f11c48a2c3a359cb19ec
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-06 14:07:32 +0430

    adding some changes

--- paginate.js
+++ paginate.js
@@ -3,8 +3,9 @@ const paginate = (followers) => {
     const numberOfPages = Math.ceil(followers.length / itemsPerPage);
     const newFollowers = Array.from({length:numberOfPages}, (_, index)=>{
         const start = index * itemsPerPage;
-        return followers.slice(start);
+        return followers.slice(start, start + itemsPerPage);
     });
+    return newFollowers;
 };
 
 
