27e5f915cf6f985daa7fbaddfd82af026a9f4faf
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-06 10:39:05 +0430

    adding some changes

--- fetchFollowers.js
+++ fetchFollowers.js
@@ -1,10 +1,9 @@
 const url = 'https://api.github.com/users/zainabturkmen/followers?per_page=100'
 
-const title = document.querySelector(".section-title h1");
 const fetchFollowers = async () => {
     const respone = await fetch(url);
-    const data = await respone.json();
-    return data;
-}
+    const data = await respone()
+    return data
+};
 
 export default fetchFollowers
