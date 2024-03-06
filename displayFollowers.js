d273e49cc92e56e1d41bb6d7512e38394026f744
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-06 11:53:37 +0430

    adding some changes

--- displayFollowers.js
+++ displayFollowers.js
@@ -3,7 +3,7 @@ const container = document.querySelector(".container")
 const display = (followers) => {
     const newFollowers = followers.map((person)=>{
         const {avatar_url, login, html, _url} = person;
-    })
-}
+    });
+};
 
 export default display
